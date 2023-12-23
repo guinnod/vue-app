e60fedce4c2941aade3bb1c04427f45f24729b5f
Karlsooon <karakatalmasova2@gmail.com>
2023-12-23 23:35:43 +0600

    commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+    <Header></Header>
+    <MenuBar></MenuBar>
+</template>
+  
+<script>
+    import Header from './components/Header.vue';
+    import MenuBar from './components/MenuBar.vue';
+
+    export default {
+        name: 'App',
+        components: {
+        Header,
+        MenuBar
+        }
+    }
+</script>
+  
+<style>
+    *{
+        box-sizing: border-box;
+        padding: 0;
+        margin:0;
+    }
+    body{
+        color: navy;
+        font-family: Verdana, Geneva, Tahoma, sans-serif;
+    }
+    
+</style>
+  
